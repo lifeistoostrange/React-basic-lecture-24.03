f1915afa36693d0db67cc0de9854cc62dbc05827
YONG <yonghyunk09@gmail.com>
2024-04-01 10:35:38 +0900

    day 4

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App12_Todo_BS';
+import App from './apps/App21_router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
