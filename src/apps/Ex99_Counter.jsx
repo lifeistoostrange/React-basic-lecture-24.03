fc91b2f37e34831a94368fb35e1a7bbaf923dc3c
YONG <yonghyunk09@gmail.com>
2024-04-02 15:07:38 +0900

    Test Example

---
+++ src/apps/Ex99_Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function Count() {
+    const [count, setCount] = useState(0);
+
+    const increaseCount = () => {
+        if (count === 10) {
+            setCount(1);
+        } else {
+            setCount(count + 1); 
+        }
+    };
+
+    const resetClick = () => {
+        setCount(1);
+    };
+
+    return (
+        <div>
+            <div>Count: {count}</div>
+            <button onClick={increaseCount}>+</button>
+            <button onClick={resetClick}>r</button>
+        </div>
+    );
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Count from "../apps/Ex99_Counter";
 
 export default function Root() {
   return (
     <div style={{margin: '20px'}}>
-      <Navbar />
-      <Outlet />
+      {/* <Navbar />
+      <Outlet /> */}
+      <Count />
     </div>
   )
 }
